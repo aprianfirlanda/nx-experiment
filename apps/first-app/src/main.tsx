689188f827eb1ab2a20671b86a3b14dcb0df2c68
aprianfirlanda <aprianfirlanda@gmail.com>
2025-07-09 22:22:12 +0700

    add nested route and layout. example is console layout

--- apps/first-app/src/main.tsx
+++ apps/first-app/src/main.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         children: [
           { index: true, element: <Dashboard /> },
           { path: 'dashboard', element: <Dashboard /> },
-          { path: 'setting', element: <Settings /> },
+          { path: 'settings', element: <Settings /> },
         ],
       },
     ],
